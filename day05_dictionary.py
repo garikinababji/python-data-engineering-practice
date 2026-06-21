2bf5295fe9b81e934f46f5b43a04bbb8195ed27a
Garikina Babji <garikina.babji@gmail.com>
2026-06-22 00:53:46 +0530

    Update and rename dictionary.py to day05_dictionary.py

---
+++ day05_dictionary.py
@@ -0,0 +1,14 @@
+# ==========================================
+# Topic   : Python dictionary
+# Author  : Garikina Babji
+# Purpose : Practice for GCP Data Engineering
+# ==========================================
+
+employee = {
+    "id": 1001,
+    "name": "Babji",
+    "experience": 4,
+    "skill": "GCP Data Engineering"
+}
+print(employee["name"])
+print(employee["experience"])
