c56b30631e985cd046101fbd4d5eaba36be28a2a
Garikina Babji <garikina.babji@gmail.com>
2026-06-22 00:22:54 +0530

    Refactor variable handling and add task comments

--- variables.py
+++ variables.py
@@ -4,12 +4,9 @@
 # Purpose : Practice for GCP Data Engineering
 # ==========================================
 
-name = input("Enter Your Name:" )
-print("Welcome to", name )
-
-
+#Task 1
 name = "Babji"
 print(f"Hello, {name}!")
-
+#Tasl 2
 name = input("Enter your name: ")
 print("Welcome", name)
