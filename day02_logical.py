9268e7f8252571d9e0ba8182a22286c02bd452ca
Garikina Babji <garikina.babji@gmail.com>
2026-06-22 01:16:32 +0530

    Update and rename logical.py to day02_logical.py

---
+++ day02_logical.py
@@ -0,0 +1,19 @@
+=============================================
+# Topic: Python logical
+# Author: Garikina Babji
+# Purpose: Practice for GCP Data Engineering
+=============================================
+# Task 1
+age = 25
+salary = 60000
+
+print(age > 18 and salary > 50000)
+print(age > 30 or salary > 50000)
+print(not(age < 18))
+
+# Task 2
+age=25
+salary = 60000
+print(age>18 and salary>50000)
+print(age>30 or salary>70000)
+print(not(age<18))
