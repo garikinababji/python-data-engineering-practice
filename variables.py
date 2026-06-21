a15ac51f990613db772f9f690b54a3fa903a19d8
Garikina Babji <garikina.babji@gmail.com>
2026-06-22 00:29:22 +0530

    Refactor variable names for clarity

--- variables.py
+++ variables.py
@@ -4,9 +4,9 @@
 # Purpose : Practice for GCP Data Engineering
 # ==========================================
 
-#Task 1
-name = "Babji"
-print(f"Hello, {name}!")
-#Task 2
+# Task 1
+user_name = "Babji"
+print(f"Hello, {user_name}!")
+# Task 2
 name = input("Enter your name: ")
 print("Welcome", name)
