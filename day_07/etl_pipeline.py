4c74d5aea09cb824c8de98958410d4c187ddefb6
Garikina Babji <garikina.babji@gmail.com>
2026-06-26 19:56:57 +0530

    Add files via upload

---
+++ day_07/etl_pipeline.py
@@ -0,0 +1,17 @@
+import csv
+
+try:
+    with open(r"D:\GCP_2026\practice\sample_csv_3.txt", "r") as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            print(row["name"])
+
+except FileNotFoundError:
+    print("Customer file not found")
+
+except KeyError:
+    print("Required column is missing")
+
+finally:
+    print("ETL Job Completed")
