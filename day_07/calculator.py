4ee45ff11c13b86bf2a0b93124d6494757e16cde
Garikina Babji <garikina.babji@gmail.com>
2026-06-26 21:13:03 +0530

    Add files via upload

---
+++ day_07/calculator.py
@@ -0,0 +1,5 @@
+
+def multiply(a, b):
+    return a * b
+import calculator
+print(calculator.multiply(10, 5))
---
+++ day_07/validation.py
@@ -0,0 +1,7 @@
+def is_valid_salary(salary):
+    return salary >= 50000
+
+import validation
+
+print(validation.is_valid_salary(60000))
+print(validation.is_valid_salary(30000))
