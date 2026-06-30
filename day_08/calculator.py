820622566498aaeea3bb57a5a7f55c2790cce80f
Garikina Babji <garikina.babji@gmail.com>
2026-06-30 13:05:14 +0530

    Add header comments to calculator.py
    Added header comments to calculator.py for documentation.

--- day_08/calculator.py
+++ day_08/calculator.py
@@ -1,4 +1,8 @@
-
+=============================================
+# Topic:  calculator
+# Author: Garikina Babji
+# Purpose: Practice for GCP Data Engineering
+=============================================
 
 def multiply(a, b):
     return a * b
