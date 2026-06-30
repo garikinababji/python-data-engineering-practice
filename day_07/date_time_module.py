b7283897099f8b261f27e5dd51f91eccb7d2e839
Garikina Babji <garikina.babji@gmail.com>
2026-06-30 13:00:09 +0530

    Print current time in date_time_module.py
    Added a print statement to display the current time.

--- day_07/date_time_module.py
+++ day_07/date_time_module.py
@@ -1,3 +1,11 @@
+=============================================
+# Topic:  date_time_module
+# Author: Garikina Babji
+# Purpose: Practice for GCP Data Engineering
+=============================================
+
+
+
 from datetime import datetime
 
 today = datetime.now()
