9e7cf59f9116cf7eb56210c31d82a197d0ae78d1
Garikina Babji <garikina.babji@gmail.com>
2026-06-30 23:13:11 +0530

    Add header comments and format code in if_else_in_list_comprehension.py

--- Python/day_10/if_else_in_list_comprehension.py
+++ Python/day_10/if_else_in_list_comprehension.py
@@ -1,3 +1,12 @@
+=============================================
+# Topic:  if_else_in_list_comprehension
+# Author: Garikina Babji
+# Purpose: Practice for GCP Data Engineering
+=============================================
+
+
+
+
 numbers = [1,2,3,4,5]
 
 result = [
