d0f9153f16f2400c6f60fc6bed5dda6a510b9b49
Garikina Babji <garikina.babji@gmail.com>
2026-06-30 13:17:50 +0530

    Add welcome method call for Customer class
    Added a welcome method call to display a greeting.

--- day_09/methods.py
+++ day_09/methods.py
@@ -1,3 +1,9 @@
+=============================================
+# Topic:   __init__
+# Author: Garikina Babji
+# Purpose: Practice for GCP Data Engineering
+=============================================
+
 class Customer:
 
     def __init__(self, name):
