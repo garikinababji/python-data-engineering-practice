c93d6480d31c6dc04cd4aa223d345f3338f4c7a4
Garikina Babji <garikina.babji@gmail.com>
2026-06-30 14:44:11 +0530

    Add day02_arithmetic.py for basic arithmetic operations

---
+++ Python/day_02 /day02_arithmetic.py
@@ -0,0 +1,13 @@
+=============================================
+# Topic: Python arithmetic
+# Author: Garikina Babji
+# Purpose: Practice for GCP Data Engineering
+=============================================
+a = 20
+b = 4
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
