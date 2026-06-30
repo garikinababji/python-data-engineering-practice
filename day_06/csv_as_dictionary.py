cfff3ad4aa5c12b97b9d7d033106bd1b0a336ca3
Garikina Babji <garikina.babji@gmail.com>
2026-06-30 12:33:44 +0530

    Add header comments to csv_as_dictionary.py

--- day_06/csv_as_dictionary.py
+++ day_06/csv_as_dictionary.py
@@ -1,3 +1,9 @@
+=============================================
+# Topic:  csv_as_dictionary
+# Author: Garikina Babji
+# Purpose: Practice for GCP Data Engineering
+=============================================
+
 import csv
 
 with open("D:\\GCP_2026\\practice\\customers_csv.txt", "r") as file:
