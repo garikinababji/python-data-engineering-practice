3ae3d592984af49906a4c9a92ddd13076fa3ec6a
Garikina Babji <garikina.babji@gmail.com>
2026-06-30 13:08:50 +0530

    Add header comments to random_module.py
    Added module header comments and restructured imports.

--- day_08/random_module.py
+++ day_08/random_module.py
@@ -1,3 +1,11 @@
+=============================================
+# Topic:  random_module
+# Author: Garikina Babji
+# Purpose: Practice for GCP Data Engineering
+=============================================
+
+
+
 import random
 
 print(random.randint(1, 10))
