1035f9f10b0e0f2e2ad1c67785b215ffdc4d9149
Garikina Babji <garikina.babji@gmail.com>
2026-06-30 12:31:28 +0530

    Update Exception Handling.py

--- day_06/Exception Handling.py
+++ day_06/Exception Handling.py
@@ -1,4 +1,8 @@
-# Exception Handling
+=============================================
+# Topic:  Exception Handling
+# Author: Garikina Babji
+# Purpose: Practice for GCP Data Engineering
+=============================================
 
 try:
     with open("D:\\GCP_2026\\practice\\customers_csv2.txt", "r") as file:
